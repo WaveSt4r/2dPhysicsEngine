6872cddbf9922ef161381a93bd9deebbe19558b7
Cava <cavasin.francesco@outlook.com>
2026-05-27 18:32:32 +0200

    removed unnecessary math

--- src/main/java/physics/collision/CollisionSolver.java
+++ src/main/java/physics/collision/CollisionSolver.java
@@ -20,9 +20,6 @@ public class CollisionSolver {
     }
 
     public static void solveCircleCircleCollision (RigidBody2D c1, RigidBody2D c2) {
-        CircleCollider cc1 = (CircleCollider) c1.collisionShape;
-        CircleCollider cc2 = (CircleCollider) c2.collisionShape;
-
         // vector that goes through both circle's centers, I'll call it Normal Vector or nV
         Vector2 nV = new Vector2 (c2.position.x - c1.position.x, c2.position.y - c1.position.y);
         nV.multiply(1d/(nV.magnitude())); // nV normalized
@@ -33,20 +30,14 @@ public class CollisionSolver {
         // we project the speed of the first circle into tV and nV to get respectively the new c1 and c2 magnitudes
         double c1nM1 = Vector2.dotProduct(c1.velocity, tV);
         double c2nM1 = Vector2.dotProduct(c1.velocity, nV);
-
-        // we multiply tV and nV with these two magnitudes to find the new velocity vectors
-        Vector2 c1nV1 = Vector2.multiply(tV, c1nM1);
-        Vector2 c2nV1 = Vector2.multiply(nV, c2nM1);
-
-        // we do the same thing with c2 but inverting nV and tV
-        nV.multiply(-1);
-        //tV.multiply(-1);
-
-        // we project the speed of the second circle into tV and nV to get respectively the new c2 and c1 magnitudes
+        // we do the same thing with c2
         double c2nM2 = Vector2.dotProduct(c2.velocity, tV);
         double c1nM2 = Vector2.dotProduct(c2.velocity, nV);
 
         // we multiply tV and nV with these two magnitudes to find the new velocity vectors
+        Vector2 c1nV1 = Vector2.multiply(tV, c1nM1);
+        Vector2 c2nV1 = Vector2.multiply(nV, c2nM1);
+        // we do the same thing with c2
         Vector2 c2nV2 = Vector2.multiply(tV, c2nM2);
         Vector2 c1nV2 = Vector2.multiply(nV, c1nM2);
 

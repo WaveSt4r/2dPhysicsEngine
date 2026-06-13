ddee62eb68cd8bdfcc7999e4076439e8173efbae
Cava <cavasin.francesco@outlook.com>
2026-06-13 18:23:05 +0200

    new constructor

--- src/main/java/render/GraphicElements2D.java
+++ src/main/java/render/GraphicElements2D.java
@@ -5,32 +5,39 @@ public class GraphicElements2D {
     public ColorRGB borderColor;
     public int borderWidth;
 
-    public GraphicElements2D (ColorRGB fillColor, ColorRGB borderColor, int borderWidth) {
+    public GraphicElements2D(ColorRGB fillColor, ColorRGB borderColor, int borderWidth) {
         this.fillColor = fillColor;
         this.borderColor = borderColor;
         this.borderWidth = borderWidth;
     }
 
     /*
-    public render.GraphicElements2D (int fillColor, int borderColor, int borderWidth) {
+    public GraphicElements2D (int fillColor, int borderColor, int borderWidth) {
         this.fillColor = Color.extractColor(fillColor);
         this.borderColor = Color.extractColor(borderColor);
         this.borderWidth = borderWidth;
     }
     */
 
-    public GraphicElements2D (ColorRGB fillColor) {
+    public GraphicElements2D(ColorRGB fillColor) {
         this.fillColor = fillColor;
-        this.borderColor = new ColorRGB(0,0,0);
+        this.borderColor = new ColorRGB(0, 0, 0);
         this.borderWidth = 0;
     }
 
     /*
-    public render.GraphicElements2D (int fillColor) {
+    public GraphicElements2D (int fillColor) {
         this.fillColor = Color.extractColor(fillColor);
         this.borderColor = new Color (0,0,0);
         this.borderWidth = 0;
     }
      */
+
+    public GraphicElements2D(ColorRGB borderColor, int borderWidth) {
+        this.fillColor = new ColorRGB(0, 0, 0);
+        this.borderColor = borderColor;
+        this.borderWidth = borderWidth;
+    }
+
 
 }

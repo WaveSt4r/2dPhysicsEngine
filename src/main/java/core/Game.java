5014143ec564bf026c2a19380402310f3c0f0b49
Cava <cavasin.francesco@outlook.com>
2026-06-13 18:22:54 +0200

    move with mouse feature

--- src/main/java/core/Game.java
+++ src/main/java/core/Game.java
@@ -1,54 +1,63 @@
 package core;
 
 import math.Vector2;
-import physics.collider.BoxCollider;
-import physics.collider.CircleCollider;
 import physics.RigidBody2D;
-import render.*;
-import render.mesh.BoxMesh;
-import render.mesh.CircleMesh;
-import scenes.Scene1;
+import render.ColorRGB;
+import render.Renderer;
+import scenes.Scene2;
 
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Game {
-    public boolean game = true;
-    public final int DT = 16;
+    public boolean running = true;
+    public final static int DT = 16;
+    public static double gravity = 0.3;
 
-    public Scene currentScene;
+    public static Scene currentScene;
+    public static RigidBody2D selectedRigidBody;
 
-    public static ColorRGB backgroundColor = new ColorRGB(15,80,115);
+    public Vector2 mousePos = new Vector2();
+    public Vector2 prevMousePos = new Vector2();
 
-    public static double gravity = 0;
+    public static ColorRGB backgroundColor = new ColorRGB(15, 80, 115);
 
-    public void start () {
-        Scene scene2 = Scene1.load();
-        currentScene = scene2;
+
+    public void start() {
+        currentScene = Scene2.load();
     }
 
 
-    public void loop () {
-//        int n = 15;
-//        int i = 0;
-//        int waitTime = 10;
-//        int t = 0;
-        while (game) {
-            // CLear Buffer
+    public void loop() {
+        int n = 1000;
+        int i = 0;
+        int waitTime = 5;
+        int t = 0;
+
+        long target = 16_666_667; // 60 FPS
+        long elapsed;
+
+        while (running) {
+            long start = System.nanoTime();
+
+            // Clear Buffer
             Renderer.clearToColor(backgroundColor);
 
+            // Spawn things
 //            t++;
 //            if(i<n && t==waitTime) {
 //                RigidBody2D ball = new RigidBody2D(
-//                        new Vector2(100, 300),
+//                        new Vector2(500, 100),
 //                        1d,
 //                        new CircleCollider(20),
-//                        new CircleMesh(20, new GraphicElements2D(new ColorRGB((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255))))
+//                        new CircleMesh(20, new GraphicElements2D(new ColorRGB(255,255,255)))
 //                );
-//                ball.velocity.set(5,0);
+//                ball.restitution = .6;
+//                ball.friction = 0.02;
+//                ball.velocity.set(Math.random()*5,Math.random()*5);
 //                currentScene.add(ball);
 //                i++;
 //                t=0;
@@ -57,6 +66,17 @@ public class Game {
             // Update
             currentScene.update();
 
+            // Move objects with mouse
+            if (Launcher.frame.getMousePosition() != null) {
+                prevMousePos.set(mousePos);
+                mousePos.set(Launcher.frame.getMousePosition().x, Launcher.frame.getMousePosition().y - 30);
+            } else {
+                selectedRigidBody = null;
+            }
+            if (selectedRigidBody != null) {
+                selectedRigidBody.velocity.set(mousePos.x - prevMousePos.x, mousePos.y - prevMousePos.y);
+            }
+
             // Draw
             currentScene.draw();
 
@@ -64,8 +84,19 @@ public class Game {
             //Renderer.fxaa();
 
             // Repaint
-            GamePanel.repaintPanel();
+            Launcher.repaintPanel();
 
+
+            // 60 FPS
+//            elapsed = System.nanoTime() - start;
+//
+//            if (elapsed < target) {
+//                try {
+//                    Thread.sleep((target - elapsed) / 1_000_000);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
 
             // 60 fps
             try {
@@ -77,14 +108,14 @@ public class Game {
     }
 
 
-    public void createKeyListeners (Frame frame) {
-        // Esci dal game loop quando si preme esc
+    public void createListeners(Frame frame) {
+        // Exit the loop when Esc key is pressed
         frame.addKeyListener(new KeyAdapter() {
             @Override
             public void keyReleased(KeyEvent e) {
                 super.keyReleased(e);
-                if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-                    game = false;
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    running = false;
                 }
             }
         });
@@ -102,5 +133,35 @@ public class Game {
 //                }
 //            }
 //        });
+
+        frame.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                Vector2 mousePos = new Vector2(e.getPoint().x, e.getPoint().y - 30);
+                Vector2 relMousePos = new Vector2();
+                for (RigidBody2D rb : currentScene.rigidBody2DList) {
+                    relMousePos.set(mousePos.x - rb.position.x, mousePos.y - rb.position.y);
+                    //System.out.println(relMousePos);
+                    if (rb.collisionShape.isPointInside(relMousePos)) {
+                        selectedRigidBody = rb;
+                        //System.out.println("Caught a wild rigid body: " + rb);
+                        break;
+                    }
+                }
+                //System.out.println("Mouse pressed");
+            }
+        });
+
+        frame.addMouseListener(new MouseAdapter() {
+            public void mouseReleased(MouseEvent e) {
+                super.mousePressed(e);
+                if (selectedRigidBody != null) {
+                    if (selectedRigidBody.isStatic) {
+                        selectedRigidBody.velocity.set(0, 0);
+                    }
+                }
+                selectedRigidBody = null;
+            }
+        });
     }
 }
